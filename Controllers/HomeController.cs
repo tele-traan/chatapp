cfe5d99335541a0f8ff0e21d760f75f73cea38c5
Timur <timurturovets@mail.ru>
2021-11-29 23:40:04 +0300

    Fixed incorrect interaction with database

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
 namespace ChatApp.Controllers
 {
     public class HomeController : Controller
@@ -17,14 +19,18 @@ namespace ChatApp.Controllers
         {
             _hub = hub; _dbContent = content;
         }
-        public IActionResult Index()
+        public IActionResult Index(string message)
         {
             int users = _dbContent.Users.Count();
-            var model = new ChatViewModel { Message = $"Привет! На данный момент в чате {users} человек" };
+            var model = new ChatViewModel { Message = message ?? $"Привет! На данный момент в чате {users} человек" };
             return View(model);
         }
         public async Task<IActionResult> Chat(ChatViewModel model)
         {
+            /*if(Regex.IsMatch(model.UserName, "_*"))
+            {
+                return RedirectToAction(actionName: "Index", routeValues: new { message="Имя должно состоять только из латинских символов и цифр :(" });
+            }*/
             string userName = model.UserName;
             _dbContent.Users.Add(new User { Username = userName });
             _dbContent.SaveChanges();
@@ -37,4 +43,4 @@ namespace ChatApp.Controllers
             return $"{id * r.Next(1, 4)},{id * r.Next(1, 4)}";
         }
     }
-}       
+}
